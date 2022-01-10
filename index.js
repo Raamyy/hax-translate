c9d875b4ae01b9d8390a2c83693e7efe98198e15
Raamyy <ramyeg26@gmail.com>
2022-01-10 19:20:25 +0200

    enabled CORs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.post('/', function (req, res) {
